b3ceb3f15539175e499e4abfc00d4890e594b42a
Saad Rana <saadrana219@gmail.com>
2019-11-05 16:07:12 -0800

    fixed logstash cm issue

--- final-deploy/logstash-manifests/deploy-logstash.sh
+++ final-deploy/logstash-manifests/deploy-logstash.sh
@@ -4,7 +4,7 @@
 yq w -i logstash-deployment.yml 'spec.template.spec.containers[0].env[1].value' $1
 
 kubectl apply -f logstash-namespace.yml
-kubectl apply -f logstash-cm.yml
+kubectl create -f logstash-cm.yml
 kubectl apply -f logstash-deployment.yml
-sleep 10
+sleep 20
 kubectl -n logstash get pod -oyaml | yq r - 'items[0].status.podIP'
